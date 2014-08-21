2831135f3825dcbad58fa44999af330f7621c2e5
Lu Shengliang <lushl9301@gmail.com>
2014-08-21 19:35:02 +0800

    Motor speed setting

---
+++ motor/src/Speed.cpp
@@ -0,0 +1,90 @@
+#include "Speed.h"
+
+Speed::Speed() {
+    ;
+}
+
+Speed::Speed(MotorShield md) {
+    _md = md;
+}
+
+void Speed::setM1SpeedLvl(int lvl) {
+    switch (lvl) {
+        case 1:
+            _md.setM1Speed(65);
+            break;
+        case 2:
+            _md.setM1Speed(130);
+            break;
+        case 3:
+            _md.setM1Speed(195);
+            break;
+        case 4:
+            _md.setM1Speed(255);
+            break;
+        case -1:
+            _md.setM1Speed(-65);
+            break;
+        case -2:
+            _md.setM1Speed(-130);
+            break;
+        case -3:
+            _md.setM1Speed(-195);
+            break;
+        case -4:
+            _md.setM1Speed(-255);
+            break;
+        default:
+            _md.setM1Speed(40);
+            break;
+    }
+}
+
+void Speed::setM2SpeedLvl(int lvl) {
+    switch (lvl) {
+        case 1:
+            _md.setM2Speed(65);
+            break;
+        case 2:
+            _md.setM2Speed(130);
+            break;
+        case 3:
+            _md.setM2Speed(195);
+            break;
+        case 4:
+            _md.setM2Speed(255);
+            break;
+        case -1:
+            _md.setM2Speed(-65);
+            break;
+        case -2:
+            _md.setM2Speed(-130);
+            break;
+        case -3:
+            _md.setM2Speed(-195);
+            break;
+        case -4:
+            _md.setM2Speed(-255);
+            break;
+        default:
+            _md.setM2Speed(40);
+            break;
+    }
+}
+
+void Speed::setSpeedLvls(int m1Lvl, int m2Lvl) {
+    setM1SpeedLvl(m1Lvl);
+    setM2SpeedLvl(m2Lvl);
+}
+
+void Speed::setM1BrakeLvls(int lvl) {
+    ;
+}
+
+void Speed::setM2BrakeLvls(int lvl) {
+    ;
+}
+
+void Speed::setBrakeLvls(int m1Lvl, int m2Lvl) {
+    ;    
+}
---
+++ motor/src/Speed.h
@@ -0,0 +1,23 @@
+#ifndef Speed_h
+#define Speed_h
+
+#include "MotorShield.h"
+
+class Speed {
+public:
+    Speed();
+    Speed(MotorShield md);
+    ~Speed();
+    void setM1SpeedLvl(int lvl);
+    void setM2SpeedLvl(int lvl);
+    void setSpeedLvls(int m1Lvl, int m2Lvl);
+    void setM1BrakeLvls(int lvl);
+    void setM2BrakeLvls(int lvl);
+    void setBrakeLvls(int m1Lvl, int m2Lvl);
+    
+private:
+    MotorShield _md;
+    
+};
+
+#endif
