552cedf5c0f9bbb95cad08ff98cc46453394cece
Lu Shengliang <lushl9301@gmail.com>
2014-10-18 14:26:48 +0800

    start new; big change

---
+++ exploration use IR sensor to turn/src/SharpA21.h
@@ -0,0 +1,27 @@
+#ifndef SharpA21_h
+#define SharpA21_h
+
+#define FilterLength 7
+
+#include "DataSmoothing.h"
+#include "Arduino.h"
+
+class SharpA21 {
+public:
+    
+    SharpA21();
+    
+    void init(int inputPin);
+    
+    int getDis();
+    int getDisCM();
+
+private:
+    int _inputPin;
+    int _inputs[FilterLength];
+    int _head;
+    DataSmoothing _ds;
+};
+
+#endif
+
